3422091da92956f0504ee74a3b900f479fd4a2fe
dongyeol02 <sprent7@gmail.com>
2026-02-11 11:13:37 +0900

    헤더 그림자 추가

--- src/component/constants/Header.tsx
+++ src/component/constants/Header.tsx
@@ -2,9 +2,10 @@ import HeaderIcon from '../../../public/icons/HedartIcon.svg';
 
 const Header = () => {
   return (
-    <div className="w-full h-[52px] bg-[#E8E2D5] flex items-center justify-between px-10">
-      <img src={HeaderIcon} />
-      Header
+    <div className="w-full h-[52px] bg-[#E8E2D5] flex items-center justify-between px-10 shadow-[0_2px_4px_rgba(0,0,0,0.1)]">
+      <img src={HeaderIcon} alt="header-icon" />
+      <span className="text-h-18b">Header</span>
+      <div className="w-6" /> {/* 우측 정렬 균형 맞추기용 */}
     </div>
   );
 };
