5f040619da9e4aead63c95816dcbd0a545b4474b
dongyeol02 <sprent7@gmail.com>
2026-02-20 10:37:58 +0900

    메인 반응형 수정

--- src/pages/home/DashboardMain.tsx
+++ src/pages/home/DashboardMain.tsx
@@ -11,13 +11,16 @@ const DashboardMain = () => {
   if (loading) {
     return <LoadingSpinner message="농장 정보를 가져오고 있습니다!" />;
   }
-
   return (
-    <div className="flex flex-col h-full bg-white">
-      <DashboardHeader nickname={nickname} />
+    <div className="pageContainer bg-white">
+      {/* 1. 헤더는 자기 높이 유지 */}
+      <div className="shrink-0">
+        <DashboardHeader nickname={nickname} />
+      </div>
 
-      {/* 하단 화이트 영역 */}
-      <div className="flex-1 px-6 pt-10 pb-10 shadow-report relative z-20 bg-white">
+      {/* 2. 하단 영역: 여기가 핵심! */}
+      <div className="flex-1 px-6 pt-10 pb-10 shadow-report relative z-20 bg-white overflow-y-auto scrollbar-hide">
+        {/* flex-1이 남은 공간을 다 먹고, 내용이 많아지면 여기서만 스크롤이 생겨 */}
         <FarmSummary summary={summary} />
         <hr className="border-[#8B8880] mb-8" />
         <FarmList farms={farms} />
