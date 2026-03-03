e3b8ec3fe77ee9478c5041e9abb0015e8fff3aa7
dongyeol02 <sprent7@gmail.com>
2026-03-03 13:26:05 +0900

    s3로직 완성

---
+++ src/hooks/useFarmImage.ts
@@ -0,0 +1,25 @@
+/**
+ * 최강동열이형, 이 훅은 서버 내부 경로를 S3 공개 주소로 바꿔주는 역할만 해!
+ * 비동기 로직(axios)이 없어서 에러가 날 확률이 거의 없어.
+ */
+export const useFarmImage = (imgUrl: string | undefined): string => {
+  // 1. 형이 알려준 S3 기본 이미지 주소
+  const DEFAULT_IMAGE =
+    'https://hansungfarmimg.s3.eu-north-1.amazonaws.com/farm/farmBasicImg.svg';
+
+  // 2. 주소 데이터가 아예 없으면 기본 이미지 리턴
+  if (!imgUrl) return DEFAULT_IMAGE;
+
+  // 3. 이미 완성된 S3 주소(http로 시작)라면 그대로 사용
+  if (imgUrl.startsWith('http')) return imgUrl;
+
+  // 4. 문제의 '/home/farmus/uploads/' 경로가 포함되어 있다면?
+  if (imgUrl.includes('/home/farmus/uploads/')) {
+    // 파일명만 쏙 빼서 S3 주소랑 합치기
+    const fileName = imgUrl.split('/').pop();
+    return `https://hansungfarmimg.s3.eu-north-1.amazonaws.com/farm/${fileName}`;
+  }
+
+  // 5. 그 외 정체를 알 수 없는 경로는 안전하게 기본 이미지로!
+  return DEFAULT_IMAGE;
+};
