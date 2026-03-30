55d78f87340fe640933794ff91b2f2806a9cb8ab
dongyeol02 <sprent7@gmail.com>
2026-03-30 09:33:49 +0900

    농장 조회 api 응답값 추가

--- src/types/farm.ts
+++ src/types/farm.ts
@@ -38,7 +38,9 @@ export interface FarmResponse {
   role: string;
   createdDate: string;
   img: string;
-  area?: number; // ✨ 추가: 면적 데이터 대응
+  area?: number;
+  cropCode: string;
+  cropsId: number;
 }
 
 export interface FarmSummary {
