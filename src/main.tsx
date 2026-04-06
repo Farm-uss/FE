925828b5ef8bf9b32523e7a0cf1bcfa5c935d50f
dongyeol02 <sprent7@gmail.com>
2026-04-07 08:58:13 +0900

    환경설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import '@/styles/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from '@/App.tsx';
+
+// ✨ 빌드 환경(운영 서버)에서만 서비스 워커를 등록하도록 설정
+if (import.meta.env.PROD) {
+  registerSW({ immediate: true });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -17,37 +17,32 @@ export default defineConfig(({ mode }) => ({
         enabled: mode === 'development',
         type: 'module',
       },
-
-      includeAssets: [
-        'icons/favicon.ico',
-        'icons/apple-touch-icon.png',
-        'icons/masked-icon.svg',
-      ],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'SpeakOn',
-        short_name: 'SpeakOn',
-        description: '9기 UMC 데모데이 프로젝트 SpeakOn',
+        name: 'Farm-us', // 전체 이름 변경
+        short_name: '팜어스', //  앱 아이콘 아래에 뜰 이름
+        description: '내 손안의 스마트 농장 관리 서비스, 팜어스', // 설명 변경
         lang: 'ko-KR',
         start_url: '/',
         scope: '/',
-        display: 'standalone',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
+        display: 'standalone', // 진짜 앱처럼 주소창 없이 실행됨
+        theme_color: '#E6E0D3', // 형이 쓰는 메인 배경색으로 변경!
+        background_color: '#E6E0D3', // 앱 실행 시 스플래시 화면 배경색
         icons: [
           {
-            src: 'icons/android-chrome-192x192.png',
+            src: 'IconPwa.svg',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/svg+xml',
           },
           {
-            src: 'icons/android-chrome-512x512.png',
+            src: 'IconPwa.svg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
           },
           {
-            src: 'icons/maskable-icon-512x512.png',
+            src: 'IconPwa.svg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
             purpose: 'maskable',
           },
         ],
